7c93f308b03acefdaea4c9a22f5658b618a3c527
hsun-wei <wilson39482433@gmail.com>
2024-05-28 00:26:16 +0800

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, ans;
+    char Operator;
+
+    cout << "請輸入第一個數字: ";
+    cin >> num1;
+
+    cout << "請輸入運算符號 (+, -, *, /): ";
+    cin >> Operator;
+
+    cout << "請輸入第二個數字: ";
+    cin >> num2;
+
+    switch (Operator) {
+    case '+':
+        ans = num1 + num2;
+        break;
+    case '-':
+        ans = num1 - num2;
+        break;
+    case '*':
+        ans = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0) {
+            ans = num1 / num2;
+        }
+        else {
+            cout << "除數不能為零";
+            return 1;
+        }
+        break;
+    default:
+        cout << "錯誤的運算符號";
+        return 1;
+    }
+
+    cout << "結果: " << ans << endl;
+    return 0;
+}
---
+++ tower.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+void tower(int n, char A, char B, char C, int& steps) {
+    if (n == 1) {   
+        printf("將盤子從 %c 移動到 %c\n", A, C);
+        steps++;
+    }
+    else {
+        tower(n - 1, A, C, B, steps);
+        printf("將盤子從 %c 移動到 %c\n", A, C);
+        steps++;
+        tower(n - 1, B, A, C, steps);
+    }
+}
+
+int main() {
+    int n;  
+    printf("請輸入盤數：");
+    scanf_s("%d", &n);
+    int steps = 0; 
+    tower(n, 'A', 'B', 'C', steps);
+    printf("總步數：%d\n", steps); 
+    return 0;
+}
